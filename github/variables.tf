51a11ccb3ffaf7a39c50c791728f04eaf202aa50
Kevin <kevinjimenezuk@gmail.com>
2025-01-14 12:50:45 +0100

    Update variables.tf

--- github/variables.tf
+++ github/variables.tf
@@ -14,3 +14,9 @@ variable "invitee_github_tokens" {
     type = list(string)
     default = ["xxxxx", "xxxxxx"] # Aqui deberian estar los tokens de los colaboradores (deben proporcionarlos ellos)
 }
+
+# Esto estaria bien automatizarlo o mejorarlo de alguna forma
+variable "tag_name" {
+    type = string
+    default = "v1.0.0"
+}
